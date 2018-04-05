4770401a6f2ce678fc568b394b3f280d24702eb6
Diego Ramos <diego@firstcontrol.com.br>
2018-04-05 13:04:02 -0300

    teste

--- www/model/mdl_login.js
+++ www/model/mdl_login.js
@@ -335,20 +335,22 @@ function perfil_notificacao(id_condominio){
             select_user(retorno[0]['id_usuario_condominio']);
             var id_reg    = localStorage.getItem("NOT_ID");
             var tipo      = localStorage.getItem("NOT_TYPE");
-            if(id_reg > 0){
-                alert('teste');
-                if(tipo==1){
-                    carrega_comunicado(id_reg);
-                }else if(tipo==2){
-                    carrega_documentos(0);
-                }else if(tipo==3){
-                    carrega_enquete(id_reg);
-                }else if(tipo==4){
-                    alert('teste2');
-                    inicia2(1);
-                    carrega_chat();
+            var intervalo = window.setInterval(function() {
+                if(id_reg > 0){
+                    alert('teste');
+                    if(tipo==1){
+                        carrega_comunicado(id_reg);
+                    }else if(tipo==2){
+                        carrega_documentos(0);
+                    }else if(tipo==3){
+                        carrega_enquete(id_reg);
+                    }else if(tipo==4){
+                        alert('teste2');
+                        inicia2(1);
+                        carrega_chat();
+                    }
                 }
-            }
+            }, 500);            
 		}
 	});
 }
