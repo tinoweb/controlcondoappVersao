f2c637ebcb1c04aa96e9619deb6087312e4f16c8
Diego Ramos <diego@firstcontrol.com.br>
2018-08-14 14:02:31 -0300

    Cameras

--- www/model/mdl_cameras.js
+++ www/model/mdl_cameras.js
@@ -7,27 +7,50 @@ function carrega_cameras(){
     var tamanhoh = (tamanhow * 0.56);
     $(".caixa_camera").width(tamanhow/2)
     $(".caixa_camera").height(tamanhoh/2);
-//	$.ajax({
-//		type: 'POST',
-//		url: localStorage.getItem('DOMINIO')+'appweb/area_comum_get.php',
-//        crossDomain: true,
-//        beforeSend : function() { },
-//        complete   : function() { },
-//        data       : {id_condominio : $( "#DADOS #ID_CONDOMINIO" ).val()},
-//        dataType   : 'json',
-//		success: function(retorno){
-// //           dados = '<div class="area col button button-big button-raised button-fill link popup-open" style=" margin-bottom: 4%;" onClick="carrega_minha_reserva(\'0\');">MINHAS RESERVAS</div>';
-//            dados = '<div class="area col button button-big button-raised button-fill link popup-open" style=" margin-bottom: 4%;" onClick="carrega_minha_reserva(\'0\');">MINHAS RESERVAS</div>';
-//            for (x in retorno) {
-//                var dado = '<div class="area" onClick="carrega_area(\''+retorno[x]['id_area_comum']+'\',\'1\',\''+retorno[x]['nome']+'\');"><strong>'+retorno[x]['nome']+'</strong></div>';
-//                var dado_select = '<option value="'+retorno[x]['id_area_comum']+'">'+retorno[x]['nome']+'</option>';
-//                dados = dados + dado;
-//                dados_select = dados_select + dado_select;
-//            }
-//			$( "#areas_retorno" ).html(dados);
-//			$( "#filtro_r_area" ).html(dados_select);
-//			afed('#reservas','#home','','',3,'reservas');
-//
-//		}
-//	});	
+    
+	$.ajax({
+		type: 'POST',
+		url: localStorage.getItem('DOMINIO')+'appweb/cameras_get.php',
+        crossDomain: true,
+        beforeSend : function() { },
+        complete   : function() { },
+        data       : {id_condominio : $( "#DADOS #ID_CONDOMINIO" ).val()},
+        dataType   : 'json',
+		success: function(retorno){
+            var ttcan = retorno.length;
+            var cont = 0;
+            var cont_can = 0;
+            dados = '';
+            for (x in retorno) {
+                cont++;
+                cont_can++;
+                if(cont_can == 1){ var grid = '<div class="row no-gap">'; var grid2 = ''; };
+                if(cont_can == 2){ var grid = ''; var grid2 = '</div>'; cont_can = 0; };
+                var dado = grid+'<div class="col"><video id="video_'+cont+'" style="width: 100%;" src="'+retorno[x]['url']+'" autoplay poster="img/bg_sem_video2.jpg" onClick="camera_play('+cont+');"></video></div>'+grid2;
+                dados = dados + dado;
+            }
+            if (ttcan % 2 === 0){
+            }else{
+                var dado = '<div class="col"><video style="width: 100%;" src="" autoplay  poster="img/bg_sem_video.jpg"></video></div></div>';
+                dados = dados + dado;
+            }
+
+            $( "#cameras_retorno" ).html(dados);
+		}
+	});	
+}
+
+function camera_play(cam){
+    var x = document.getElementById("video_"+cam);
+    if (x.paused){
+        x.load();
+    }else{
+        if(x.requestFullScreen){
+            x.requestFullScreen();
+        } else if(x.webkitRequestFullScreen){
+            x.webkitRequestFullScreen();
+        } else if(x.mozRequestFullScreen){
+            x.mozRequestFullScreen();
+        }
+    }
 }
