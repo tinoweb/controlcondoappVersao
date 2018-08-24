5a4c1ead98640ebcb693866c8398a58de27c4fbf
Diego Ramos <diego@firstcontrol.com.br>
2018-08-24 17:22:46 -0300

    Correcao login deley

--- www/model/mdl_login.js
+++ www/model/mdl_login.js
@@ -368,7 +368,7 @@ function logout(){
 }
 
 function perfil_notificacao(id_condominio){
-    alert($( "#DADOS #ID_USER_L" ).val());
+    //alert($( "#DADOS #ID_USER_L" ).val());
 	$.ajax({
 		type: 'POST',
 		url: localStorage.getItem('DOMINIO')+'appweb/usuario_perfil_get.php',
