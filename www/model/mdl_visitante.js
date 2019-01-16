0cd7bd662d1318140a409841548e74ad5584c0b5
Caio Souza <caio.souza@firstcontrol.com.br>
2019-01-16 18:53:18 -0200

    ocorrencia

--- www/model/mdl_visitante.js
+++ www/model/mdl_visitante.js
@@ -57,6 +57,9 @@ function escolhe_visita(id,nome,rg){
 	afed('#liberacao2','#visitantes','','',3,'liberacao_add');
 	$( "#add_liberacao #savarLib").html('<span class="fa fa-check"></span> Salvar');
 	$("#liberacao_placa").val("");
+	$("#cad_veiculo").hide();
+	
+	
 
 }
 
