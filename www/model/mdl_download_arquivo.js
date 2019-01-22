c75d5c7ac59b73e2594e9bbf53930db7d1880567
Caio Souza <caio.souza@firstcontrol.com.br>
2019-01-22 11:43:36 -0200

    download

--- www/model/mdl_download_arquivo.js
+++ www/model/mdl_download_arquivo.js
@@ -44,10 +44,9 @@ function download_arquivo(path,tipo,nome){
             console.log("download error source " + error.source);
             console.log("download error target " + error.target);
 			alert('error');
-			alert(uri);
-			alert(filePath);
+			alert(error);
 			
-            //console.log("upload error code" + error.code);
+          
         },
         false,
         {
