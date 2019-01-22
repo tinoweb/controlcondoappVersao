012088e2191ef421f7e8fbfd54b79e10900d7efd
Caio Souza <caio.souza@firstcontrol.com.br>
2019-01-22 14:55:58 -0200

    download

--- www/model/mdl_download_arquivo.js
+++ www/model/mdl_download_arquivo.js
@@ -38,18 +38,11 @@ function download_arquivo(path,tipo,nome){
 			ref.addEventListener('loadstart', myCallback);
 			ref.addEventListener('loaderror', myCallback);
 			ref.removeEventListener('loadstart', myCallback);
-			//window.open(path, "_system");
+			window.open(filePath, "_system");
 			//alert('sucesso');
 		    //alert(filePath);
 			
-			   function readDataUrl(uri) {
-        var reader = new FileReader();
-        reader.onloadend = function(evt) {
-            console.log("Read as data URL");
-            console.log(evt.target.result);
-        };
-        reader.readAsDataURL(file);
-    }
+			
 			
             
 
