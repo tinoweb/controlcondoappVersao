4a8015d3b4c65db532f93fe7a60ceb4f881fcc30
Caio Souza <caio.souza@firstcontrol.com.br>
2019-01-23 09:18:42 -0200

    download

--- www/model/mdl_download_arquivo.js
+++ www/model/mdl_download_arquivo.js
@@ -46,7 +46,7 @@ function download_arquivo(path,tipo,nome){
 			//alert('sucesso');
 		    //alert(filePath);*/
 			
-			var ref = cordova.InAppBrowser.open(uri, '_system', 'location=yes');
+			var ref = cordova.InAppBrowser.open(uri, '_system', 'hideurlbar=yes');
 			// some time later...
 			
 			/*setTimeout(function(){
