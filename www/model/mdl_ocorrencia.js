8cf40ae89db17e2c5d5ae596355565626e3de987
Caio Souza <caio.souza@firstcontrol.com.br>
2019-01-23 10:35:53 -0200

    ocorrencia

--- www/model/mdl_ocorrencia.js
+++ www/model/mdl_ocorrencia.js
@@ -341,7 +341,7 @@ function download_arq_ocorrencia(arquivo) {
     //var uri = encodeURI("http://portal.mec.gov.br/seb/arquivos/pdf/Profa/apres.pdf");
     var uri = encodeURI(path);
 	
-    var filePath = cordova.file.externalRootDirectory+'Download/'+arquivo;
+    var filePath = cordova.file.externalApplicationStorageDirectory+'Download/'+arquivo;
 	fileTransfer.onprogress = function(progressEvent) {
 		if (progressEvent.lengthComputable) {
 			var perc = Math.floor(progressEvent.loaded / progressEvent.total * 100);
@@ -359,7 +359,7 @@ function download_arq_ocorrencia(arquivo) {
             //notifica('Download/Download Concluído90 /ok',0,0);
 			var path = entry.toURL(); //**THIS IS WHAT I NEED**
 			//alert(path);
-			var ref = cordova.InAppBrowser.open(path, '_system', 'location=yes');
+			var ref = cordova.InAppBrowser.open(uri, '_system', 'location=yes');
 			//alert(JSON.stringify(ref, null, 4));
             //window.open(path, "_system");
 
