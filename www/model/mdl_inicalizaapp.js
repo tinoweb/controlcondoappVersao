1c07eec9f1a9801341f8a3663e93336f27e70214
tino <tino@firstcontrol.com>
2019-10-31 18:17:16 -0300

    finanlizando os recursos

--- www/model/mdl_inicalizaapp.js
+++ www/model/mdl_inicalizaapp.js
@@ -14,13 +14,92 @@ function primeiroAcessoBtnVoltar(){
 
 function swich_tela_primeiroAcesso(){
 	afed('#primeiroAcesso','#initApp','','',1);
+
+}
+
+function emailNotRecognizedBySystemAlert(type, messenge, afterClose=null){
+	Swal.fire({
+	  	type: type,
+	  	text: messenge,
+		timer: 4000,
+		onBeforeOpen: () => {
+			Swal.showLoading()
+			timerInterval = setInterval(() => {}, 100)
+		},
+		onClose: () => {
+			if (afterClose == "primeiroAcesso") {
+				$("#inputReceveEmailToGetCode").val("");
+				$("#telaVerificaCodigo").css('display', 'block');
+				$("#primeiroAcesso").css('display', 'none');
+				$("#initApp").css('display', 'none');
+			}else if(afterClose == "defineSenha"){
+				switchTelaDefineSenhaToLogin();
+			}
+		}
+	}).then((result) => {
+		if (result.dismiss === Swal.DismissReason.timer) {
+			// console.log('I was closed by the timer');
+		}
+	});
+}
+
+function alertShowPosibilityToResetPassword(){
+	Swal.fire({
+		// title: "Are you sure?",
+	  	text: "Esse email se encontra ativo! Deseja recuperar a sua senha",
+	  	type: 'warning',
+	  	showCancelButton: true,
+	  	confirmButtonColor: '#3085d6',
+	  	cancelButtonColor: '#d33',
+	  	confirmButtonText: 'Sim recuperar senha!'
+	}).then((result) => {
+	  	if (result.value) {
+		    // após aceitar encaminhar 
+		    // para a tela de resetar a senha 
+	  	}
+	});
 }
 
 
 function choosedMail(){
-	$("#telaVerificaCodigo").css('display', 'block');
-	$("#primeiroAcesso").css('display', 'none');
-	$("#initApp").css('display', 'none');
+	
+	let campoEmail = $("#inputReceveEmailToGetCode").val();
+	if (campoEmail.length !== 0) {
+		console.log(localStorage.getItem('DOMINIO')+'appweb/ativacao_post.php');
+		$.ajax({
+			type: 'POST',
+			url: localStorage.getItem('DOMINIO')+'appweb/ativacao_post.php',
+			crossDomain: true,
+			beforeSend : function() { $("#wait").css("display", "block"); },
+			complete   : function() { $("#wait").css("display", "none"); },
+	        data       : { email : campoEmail, typeFunction : 'enviarEmailParaAtivacao' },
+	        dataType   : 'json',
+			success: function(retorno){
+				if (retorno.status == "emailNaoReconhecidoPeloSistema") {
+					app2.sheet.close('.recebEmail', true);
+					emailNotRecognizedBySystemAlert('error','Email não reconhecido pelo sistema, Insira seu email cadastrado');
+					$("#inputReceveEmailToGetCode").val("");
+				}else if(retorno.status == "naoPossuiNenhumPerfilAtivo"){
+					app2.sheet.close('.recebEmail', true);
+					emailNotRecognizedBySystemAlert('error','Esse email não possui perfil Ativo no sistema');
+					$("#inputReceveEmailToGetCode").val("");
+				}else if (retorno.status == "codigoEnviadoParaEmailComSucesso" && retorno.statuscode == 200) {
+					app2.sheet.close('.recebEmail', true);
+					emailNotRecognizedBySystemAlert('success', "Código de Ativação enviado para o email com Sucesso", "primeiroAcesso");
+				}else if (retorno.status == "codigoEnviadoParaEmailComSucesso" && retorno.statuscode == 200) {
+					app2.sheet.close('.recebEmail', true);
+
+				}
+		
+	        },
+	        error: function(error) {
+	        	// alert("tem informacoes com erro");
+				console.log(error);
+	        }
+		});	
+	}else{
+		alerta("","Insira seu email para continuar", 3000);	
+	}
 }
 
 function choosedSms(){
@@ -35,37 +114,45 @@ function voltaraoPrimeiroAcesso(){
 }
 
 function swich_to_primeiroAcesso(){
-	afed('#primeiroAcesso','#telaVerificaCodigo','','',1);	
+	afed('#primeiroAcesso','#telaVerificaCodigo','','');	
+	// $("#btnSendEmailToGetCode").attr('disabled', true);
+
 }
 
 function enviarCodigoAtivacao(){
 	let codigoAtivacao = $("#codigoAtivacao").val();
 	console.log(localStorage.getItem('DOMINIO')+'appweb/ativacao_post.php');
-
-	$.ajax({
-		type: 'POST',
-		url: localStorage.getItem('DOMINIO')+'appweb/ativacao_post.php',
-		crossDomain: true,
-		beforeSend : function() { $("#wait").css("display", "block"); },
-		complete   : function() { $("#wait").css("display", "none"); },
-        data       : { codigo : codigoAtivacao, typeFunction : 'enviarCodigoAtivacao' },
-        dataType   : 'json',
-		success: function(retorno){
-			console.log(retorno);    
-			if (retorno.statuscode == 200 && retorno.status == "codigoOk") {
-				localStorage.setItem("idUsuarioAtivacao", retorno.idUsuario);
-				$("#btnCancelarConta").hide();
-				$("#btnAtivarConta").hide();
-				$("#telaVerificaCodigo").hide();
-				$("#telaAceitaTermo").show();
-				$(".aceitaTermoClass").css('display', 'block');
-				$("#concordaComTermo").hide();
-			}
-        },
-        error: function(error) {
-			console.log(error);
-        }
-	});	
+	if (codigoAtivacao.length !== 0) {
+		$.ajax({
+			type: 'POST',
+			url: localStorage.getItem('DOMINIO')+'appweb/ativacao_post.php',
+			crossDomain: true,
+			beforeSend : function() { $("#wait").css("display", "block"); },
+			complete   : function() { $("#wait").css("display", "none"); },
+	        data       : { codigo : codigoAtivacao, typeFunction : 'enviarCodigoAtivacao' },
+	        dataType   : 'json',
+			success: function(retorno){
+				console.log(retorno);    
+				if (retorno.statuscode == 200 && retorno.status == "codigoOk") {
+					localStorage.setItem("idUsuarioAtivacao", retorno.idUsuario);
+					$("#btnCancelarConta").hide();
+					$("#btnAtivarConta").hide();
+					$("#telaVerificaCodigo").hide();
+					$("#telaAceitaTermo").show();
+					$(".aceitaTermoClass").css('display', 'block');
+					$("#concordaComTermo").hide();
+				}else if (retorno.statuscode == 204 && retorno.status == "usuarioNaoEncontradoParaCodigo") {
+					emailNotRecognizedBySystemAlert('error', "Código de Ativação Inválido, Confira o codigo enviado no seu email");
+					$("#codigoAtivacao").val("");
+				}
+	        },
+	        error: function(error) {
+				console.log(error);
+	        }
+		});	
+	}else{
+		app2.input.validate("#codigoAtivacao");
+	}
 }
 
 function aceiteiTermo(){
@@ -139,26 +226,12 @@ function salvarSenha(){
 				success: function(retorno){
 					if (retorno.statuscode == 200 && retorno.status == "senhaDefinidoOk") {
 						$('#formSendDefineSenha').trigger("reset");
-
-						var toastSuccess = app2.toast.create({
-							text: 'Senha definida com sucesso',
-						  	closeButton: true,
-						  	closeButtonText: 'Ok',
-						  	closeTimeout: 2000,
-							on: {
-								closed: function () {
-									switchTelaDefineSenhaToLogin();
-								  	// app2.dialog.alert('vai para outra tela...')
-								},
-							}
-						});
-
-						toastSuccess.open();
+						emailNotRecognizedBySystemAlert('success', "Senha definida com sucesso", "defineSenha");
 					}
 		        },
 		        error: function(error) {
 					console.log(error);
-					alert('eu erro >> ver console debug...');
+					alert('Não foi possivel executar a ação pretendida, entre em contato com seu administrador');
 		        }
 			});	
 		}
